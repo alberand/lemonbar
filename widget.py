d320c78d61c6540a04532f509f4098e8132bb1d8
alberand <snashe1@gmail.com>
2016-11-28 16:29:05 +0100

    Action implemented. Output implemented.

--- widget.py
+++ widget.py
@@ -27,19 +27,39 @@ class Widget:
         self.icon_p = 0
 
         self.colors_rules = dict()
+        self.action = []
+        self.action_buttons = []
 
     def update(self):
         '''
-        Update widget status.
+
+        TO IMPLEMENT.
+
         '''
         pass
 
-    def action(self):
+    def add_action(self, button, cmd):
         '''
         Implement if widget should execute any aciont, commands, programs...
+        Args:
+            button: int. A number ranging from 1 to 5 which maps to the left,
+                middle, right, scroll up and scroll down movements.
+            cmd: string. Bash command.
         '''
+        if not isinstance(button, int):
+            return None
 
-    def set_action(self, string)
+        if not button in range(1, 6):
+            return None
+
+        self.action.append(cmd)
+        self.action_buttons.append(button)
+
+    def remove_action(self, cmd):
+        self.action_buttons.pop(self.action.index(cmd))
+        self.action.remove(cmd)
+
+    def set_action(self, string, action, button):
         return '%{{A{1}:{2}:}}{0}%{{A}}'.format(string, button, action)
 
     def set_bg(self, string):
@@ -48,13 +68,31 @@ class Widget:
     def set_fg(self, string):
         return '%{{F{1}}}{0}%{{F{1}}}'.format(string, self.fg)
 
+    def get_action(self, string):
+        for cmd, button in zip(self.action, self.action_buttons):
+            string = self.set_action(string, cmd, button)
+
+        return string
+
     def get_output(self):
         '''
         Returns generated string for lemonbar.
         '''
-        return self.set_fg(' {} '.format(self.name))
+        string = ' {} {} '.format(self.icon, self.name)
+        if self.bg:
+            string = self.set_bg(string)
+            
+        if self.fg:
+            string = self.set_fg(string)
+
+        if len(self.action):
+            string = self.get_action(string)
+
+        return string
 
 if __name__ == '__main__':
     a = Widget('a')
+    a.add_action(3, 'date')
+    a.add_action(1, 'time')
 
     print(a.get_output())
