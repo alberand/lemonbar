d381a13fbeb961d0dc8b46c3569c6633adf340c9
Albershteyn Andrey <snashe1@gmail.com>
2017-01-16 15:18:44 +0100

    script: Add check if fifo files already exist.

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-mkfifo fifo0 fifo1
-python ./bar.py > fifo0 < fifo1 &
-lemonbar -p -f "Droid Sans-9" -f "FontAwesome-11" -g1366x22 eDP1 < fifo0 > fifo1
+[[ -r p_to_lb ]] || mkfifo p_to_lb
+[[ -r lb_to_p ]] || mkfifo lb_to_p
+python ./bar.py > p_to_lb < lb_to_p &
+lemonbar -p -f "Droid Sans-9" -f "FontAwesome-11" -g1366x22 eDP1 < p_to_lb > lb_to_p
 
 trap 'kill $(jobs -p)' EXIT
