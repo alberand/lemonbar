9a4bab260df97b9d2f506888602bd3e55bfa7056
Albershteyn Andrey <snashe1@gmail.com>
2017-01-29 12:24:16 +0100

    script: Move FIFO queues to /tmp.

--- run.sh
+++ run.sh
@@ -8,18 +8,18 @@
 DIRECTORY="$( cd "$( dirname "$0" )" && pwd )"
 
 # Create FIFO files to communicate between processes
-[[ -r p_to_lb ]] || mkfifo p_to_lb
-[[ -r lb_to_p ]] || mkfifo lb_to_p
+[[ -r /tmp/p_to_lb ]] || mkfifo /tmp/p_to_lb
+[[ -r /tmp/lb_to_p ]] || mkfifo /tmp/lb_to_p
 
 trap 'kill -TERM $python_pid' TERM INT
 # Run python script
-python $DIRECTORY/bar.py > p_to_lb < lb_to_p &
+python $DIRECTORY/bar.py > /tmp/p_to_lb < /tmp/lb_to_p &
 python_pid=$!
 
 echo "PID of python process: $python_pid"
 
 # Run lemonbar
-lemonbar -p -f "Droid Sans-9" -f "FontAwesome-11" -g1366x22 eDP1 < p_to_lb > lb_to_p
+lemonbar -p -f "Droid Sans-9" -f "FontAwesome-11" -g1366x22 eDP1 < /tmp/p_to_lb > /tmp/lb_to_p
 
 # Kill all when exiting
 # Lemonbar will automaticly terminates when lost input connection
@@ -29,4 +29,4 @@ trap - TERM INT
 wait $python_pid
 EXIT_STATUS=$?
 
-rm  p_to_lb lb_to_p
+rm  /tmp/p_to_lb /tmp/lb_to_p
