26592fc346f0ec02653be9c3914d91e1883fc365
Albershteyn Andrey <snashe1@gmail.com>
2017-03-19 16:16:48 +0100

    wids, battery: Add notifications about status.

--- widgets/battery.py
+++ widgets/battery.py
@@ -29,6 +29,29 @@ class battery(Widget):
         self.gaps = (10, 7)
         self.charge = 0
         self.show_text = False
+        # 0: Normal. (charge > 30% or Plugged)
+        # 1: Unplugged. (charge > 30% and unplugged)
+        # 2: 30% limit. (charge < 30% and unplugged)
+        # 3: 10% limit. (charge < 10% and unplugged)
+        # 4: Charging. (charge < 30% and plugged)
+        self.status = 0
+
+    def notify(self, title, message, priority=None):
+        '''
+        Send system notification. 
+        Args:
+            title: string
+            message: string
+            priority: string: low, normal, critical
+        '''
+        if not priority:
+            priority = 'normal'
+        subprocess.call(['notify-send', 
+            '-u',
+            '{}'.format(priority),
+            '{}'.format(title),
+            '{}'.format(message),
+        ])
 
     def update(self):
         '''
@@ -41,6 +64,7 @@ class battery(Widget):
 
         string = output.decode('UTF-8')
 
+        # Set tray icon and color
         self.charge = int(re.match('[0-9]+', string).group(0))
         if len(string) < 7 or string[-3:] == 'CHR':
             self.icon = icons['plug']
@@ -63,6 +87,30 @@ class battery(Widget):
         else:
             self.icon = icons['battery_full']
             self.fg = colors['c_white']
+
+        # Change status
+        if self.status == 0 and self.charge > 26 and len(string) == 7:
+            print("Change status.", file=sys.stderr)
+            self.notify("Battery", 
+                "Cable unplugged. Charge: {}%.".format(self.charge))
+            self.status = 1
+        elif self.status == 1 and self.charge < 30:
+            self.status = 2
+        elif self.status == 3 and self.charge < 10:
+            subprocess.call("Battery", 
+                    "Low battery: {}%.".format(self.charge),
+                    "critical")
+            self.status = 4
+        elif self.status == 2 and self.charge < 30:
+            subprocess.call("Battery",
+                "Low battery: {}%.".format(self.charge))
+            self.status = 3
+        elif self.status == 4 and string[-3:] == 'CHR':
+            subprocess.call("Battery",
+                "Cable is plugged in. Low battery: {}%.".format(self.charge))
+        else:
+            pass
+
 
         if self.show_text:
             self.value = str(self.charge) + '%'
